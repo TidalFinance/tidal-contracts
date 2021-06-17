21182c3eb96c6d21b2a52b9efa17519329ce4932
john <john@johndeMacBook-Air.local>
2021-06-17 17:58:22 +0800

    fix

--- contracts/helper/UpdateHelper.sol
+++ contracts/helper/UpdateHelper.sol
@@ -4,8 +4,8 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "../interfaces/IRegistry.sol";
-import "../Seller.sol";
-import "../Guarantor.sol";
+import "../interfaces/ISeller.sol";
+import "../interfaces/IGuarantor.sol";
 
 contract UpdateHelper is Ownable {
     
@@ -19,7 +19,7 @@ contract UpdateHelper is Ownable {
     }
     
     function update(address who_) external {
-        Seller(registry.seller()).update(who_);
-        Guarantor(registry.guarantor()).update(who_);
+        ISeller(registry.seller()).update(who_);
+        IGuarantor(registry.guarantor()).update(who_);
     }
 }

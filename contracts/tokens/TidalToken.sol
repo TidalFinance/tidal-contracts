9f8aec06333d52941f97d94877c275b7cf7b2cc2
shipunyc <mr.pu.shi@gmail.com>
2021-06-25 02:41:50 +0800

    Let TidalToken supports EIP 2771.

--- contracts/tokens/TidalToken.sol
+++ contracts/tokens/TidalToken.sol
@@ -3,11 +3,28 @@ pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// This token is owned by Timelock.
-contract TidalToken is ERC20("Tidal Token", "TIL") {
+import "../common/BaseRelayRecipient.sol";
 
-    constructor() public {
+import "../interfaces/IRegistry.sol";
+
+// This token is owned by Timelock.
+contract TidalToken is ERC20("Tidal Token", "TIL"), BaseRelayRecipient {
+
+    string public override versionRecipient = "1.0.0";
+
+    IRegistry public registry;
+
+    constructor(IRegistry registry_) public {
+        registry = registry_;
         _mint(msg.sender, 2e28);  // 20 billion, 18 decimals
+    }
+
+    function _msgSender() internal override(Context, BaseRelayRecipient) view returns (address payable) {
+        return BaseRelayRecipient._msgSender();
+    }
+
+    function _trustedForwarder() internal override view returns(address) {
+        return registry.trustedForwarder();
     }
 
     function burn(uint256 _amount) external {
